4d8697d791c04c4e34219bec5edd1195d79b6a0b
Jessica Szejer <jessicaszejer@WL-193-80.WPA.Claremont.Edu>
2014-11-15 03:12:10 -0800

    all da code

--- CollegeLyfe/FirstViewController.swift
+++ CollegeLyfe/FirstViewController.swift
@@ -13,10 +13,88 @@
 import UIKit
 
 class FirstViewController: UIViewController {
+    
+    var startTime = NSTimeInterval()
+    
+    
+    
 
+    @IBOutlet weak var percent1: UITextField!
+    @IBOutlet weak var finishtime1: UITextField!
+    @IBOutlet weak var inputtime1: UITextField!
+    @IBOutlet weak var progressbar1: UIProgressView!
+    
+    @IBOutlet weak var inputtime2: UITextField!
+    @IBOutlet weak var finishtime2: UITextField!
+    @IBOutlet weak var progressbar2: UIProgressView!
+    @IBOutlet weak var percent2: UITextField!
+    
+    @IBOutlet weak var inputtime3: UITextField!
+    @IBOutlet weak var finishtime3: UITextField!
+    @IBOutlet weak var progressbar3: UIProgressView!
+    @IBOutlet weak var percent3: UITextField!
+    
+    @IBOutlet weak var inputtime4: UITextField!
+    @IBOutlet weak var finishtime4: UITextField!
+    @IBOutlet weak var percent4: UITextField!
+    @IBOutlet weak var progressbar4: UIProgressView!
+    
+    
+    func updateTime() {
+        var currentTime = NSDate.timeIntervalSinceReferenceDate()
+        
+        //Find the difference between current time and start time.
+        var elapsedTime: NSTimeInterval = currentTime - startTime
+        
+        //calculate the minutes in elapsed time.
+        let minutes = UInt8(elapsedTime / 60.0)
+        elapsedTime -= (NSTimeInterval(minutes) * 60)
+        
+        //calculate the seconds in elapsed time.
+        let seconds = UInt8(elapsedTime)
+        elapsedTime -= NSTimeInterval(seconds)
+        
+        //find out the fraction of milliseconds to be displayed.
+        let fraction = UInt8(elapsedTime * 100)
+        
+        //add the leading zero for minutes, seconds and millseconds and store them as string constants
+        let strMinutes = minutes > 9 ? String(minutes): "0" + String(minutes)
+        let strSeconds = seconds > 9 ? String(seconds): "0" + String(seconds)
+        let strFraction = fraction > 9 ? String(fraction): "0" + String(fraction)
+        
+        //concatenate minuets, seconds and milliseconds as assign it to the UILabel
+        var timePassedString = "\(strMinutes):\(strSeconds):\(strFraction)"
+        
+        let timePassed = elapsedTime/60.0
+        
+        progressbar1.progress = (timePassed)/(1.0*inputtime1)
+        percent1.value = integer of progressbar1.value * 100
+        
+        if percent1.value == 100 {
+            set off alarm
+        }
+        
+        finishtime1 = realworldtime + inputtime1
+    }
+    
+    var timer = NSTimer()
+    
+    
+    @IBAction func go1(sender: AnyObject) {
+        let aSelector : Selector = "updateTime"
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.01, target: self, selector: aSelector, userInfo: nil, repeats: true)
+        startTime = NSDate.timeIntervalSinceReferenceDate()
+    }
+    @IBAction func stop1(sender: AnyObject) {
+        timer.invalidate()
+        timer == 0
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        refreshUI()
     }
 
     override func didReceiveMemoryWarning() {
