873f228587279fac08b8c4b38ed8624fb518ea2e
Alex Rich <arich@hmc.edu>
2014-11-14 21:50:37 -0800

    Initial Commit

---
+++ CollegeLyfe/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  CollegeLyfe
+//
+//  Created by Alex Rich on 11/14/14.
+//  Copyright (c) 2014 Alex Rich and Jessica Szejer. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ CollegeLyfe/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  CollegeLyfe
+//
+//  Created by Alex Rich on 11/14/14.
+//  Copyright (c) 2014 Alex Rich and Jessica Szejer. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
